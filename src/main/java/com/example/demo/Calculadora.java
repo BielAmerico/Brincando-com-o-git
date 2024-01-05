d7786d13e461a1d90bb2d2ec1c9ccc9fbfcb6ff8
Gabriel Americo <bielgan@hotmail.com>
2024-01-04 21:24:11 -0300

    Deletado a soma, atualizado a divisao

--- src/main/java/com/example/demo/Calculadora.java
+++ src/main/java/com/example/demo/Calculadora.java
@@ -5,13 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Calculadora {
 
-	public int somar(int x , int y) {
-		return x + y;
-	}
-	
-	public int divide(int x, int y) {
+	public int divide(int x , int y) {
 		return x / y;
 	}
+	
+	
 }
 
 

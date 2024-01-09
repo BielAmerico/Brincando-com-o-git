653af966df20c678e5376326f8ce4a0a5e19defe
Gabriel Americo <bielgan@hotmail.com>
2024-01-08 21:35:38 -0300

    Criando uma classe de multiplicação

---
+++ src/main/java/com/example/demo/CalculadoraMultiplicação.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraMultiplicação {
+
+	public int multiplica(int x , int y) {
+		return x * y;
+		
+	}
+}
